8ede4292eab3b00a12f2135092706e8ac05d057b
alexcodeninja <aalexdeada@gmail.com>
2023-10-25 03:36:38 +0200

    added:  README.md,  connect(wrap for requests firebase),  started main,  data files,  requirements.txt  empty questions_btns.py for simple questions

---
+++ connect.py
@@ -0,0 +1,26 @@
+# This file is wrap for requests
+# and needs to simplify working with the database
+import requests
+
+db_url = "firebase link"
+
+def patch_data(url_data=str, **data):
+    requests.patch(url=db_url+url_data+"/.json", json=data)
+
+def post_data(url_data=str, **data):
+    requests.post(url=db_url+url_data+"/.json", json=data)
+
+def delete_data(url_data=str, **data):
+    requests.delete(url=db_url+url_data+"/.json", json=data)
+
+def put_data(url_data=str, **data):
+    requests.put(url=db_url+url_data+"/.json", json=data)
+
+def get_data(url_to_data=str):  # Получает данные пользователя из users по логину
+    return requests.get(f"{db_url}/{url_to_data}/.json").json()
+
+def is_connected():
+    try:
+        return requests.get("https://amn-project-b3b8c-default-rtdb.europe-west1.firebasedatabase.app").ok
+    except requests.ConnectionError:
+        return False
--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-#nothing there :)
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+from tinydb import TinyDB, Query
+import googletrans
+
+logging.basicConfig(level=logging.INFO, filename="bot_logs.log", filemode="w", format="%(asctime)s %(levelname)s %(message)s")
+bot = Bot(token="6414216172:AAESR33MikSbdNKLltT02w2fMZBtwKkyAYs")
+dp = Dispatcher(bot)
+users = TinyDB("data/users_ids.json")
+questions = TinyDB("data/irregular_questions.json")
+qur = Query()
+
+@dp.message(Command("start"))
+async def start(message: types.Message):
+	if users.search(qur.id == message.from_user.id) == []:
+		bot.send_message("Добро пожаловать!\nПожалуйста выберите язык.\n\nWelocome!\nPlease choose the language.")
+		logging.info("New user started!")
+
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
